457c458d17e38a4003e0873e9bcda60f31ac7228
dorinbotan <dorinbotan@mail.ru>
2017-11-15 23:27:15 +0100

    Added REST instructions table. Implemented data update in Greenhouse app.

--- Greenhouse/app/src/main/java/com/example/dorin/greenhouse/Greenhouse.java
+++ Greenhouse/app/src/main/java/com/example/dorin/greenhouse/Greenhouse.java
@@ -1,12 +1,27 @@
 package com.example.dorin.greenhouse;
 
-public class Greenhouse {
-    public Greenhouse() {
+import java.io.IOException;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class Greenhouse {
+    private OkHttpClient client;
+
+    public Greenhouse() {
+        client = new OkHttpClient();
     }
 
-    public int getHumidity() {
-        return 0;
+    public String getHumidity() throws IOException {
+        Request request = new Request.Builder()
+                .url("http://192.168.87.116/humidity")
+                .build();
+
+//        Response response = client.newCall(request).execute();
+//        return response.body().string();
+
+        return "50 %";
     }
 
     public int getTemperature() {
--- Greenhouse/app/src/main/java/com/example/dorin/greenhouse/MainActivity.java
+++ Greenhouse/app/src/main/java/com/example/dorin/greenhouse/MainActivity.java
@@ -1,19 +1,49 @@
 package com.example.dorin.greenhouse;
 
 import android.content.Context;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    private Greenhouse greenhouse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Greenhouse greenhouse;
+        greenhouse = new Greenhouse();
 
+        final TextView humidityValue = (TextView)findViewById(R.id.humidityValueText);
+        final TextView temperatureValue = (TextView)findViewById(R.id.temperatureValueText);
+        final TextView lightValue = (TextView)findViewById(R.id.lightValueText);
+
+        final Handler handler = new Handler();
+        final int delay = 1000; //milliseconds
+
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                Random generator = new Random();
+
+                humidityValue.setText(Integer.toString(generator.nextInt(100) + 1) + " %");
+                temperatureValue.setText(Integer.toString(generator.nextInt(100) + 1) + " °C");
+                lightValue.setText(Integer.toString(generator.nextInt(100) + 1) + " %");
+
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
+    }
+
+    public void humidityButtonClick(View view) {
         Context context = getApplicationContext();
         CharSequence text = "Hello toast!";
         int duration = Toast.LENGTH_SHORT;
@@ -21,8 +51,18 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
 
-        System.out.println("--------------------------");
+        LinearLayout humidityDropdown = (LinearLayout) findViewById(R.id.humidityDropdown);
+        RelativeLayout temperatureButton = (RelativeLayout) findViewById(R.id.temperatureButton);
 
-//        findViewById(android.R.id.content).findViewById(R.id.temperatureButton).setBackgroundColor(getResources().getColor(R.color.colorAccentRed));
+        if(humidityDropdown.getVisibility() == View.GONE) {
+            humidityDropdown.setVisibility(View.VISIBLE);
+            humidityDropdown.animate().alpha(1.0f);
+            temperatureButton.animate().translationY(temperatureButton.getHeight());
+        }
+        else {
+            humidityDropdown.setVisibility(View.GONE);
+            humidityDropdown.animate().alpha(0.0f);
+            temperatureButton.animate().translationY(0);
+        }
     }
 }
